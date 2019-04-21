654e8b04383d0127f394bfa9ff4ad89b80814c3c
John Todd <jqt3of5@gmail.com>
2019-04-20 23:23:07 -0600

    Much better integration handling..

--- PID/pid.cpp
+++ PID/pid.cpp
@@ -1,17 +1,15 @@
 #include "pid.h"
 #include <math.h>
 
-#define SAMPLES 1000
-
-float Kp = 10;
-float Ki = .1;
+float Kp = 8;
+float Ki = .02;
 float Kd = 4;
 float _sp = 0;
 
-int _measurements = 0;
-int _index;
-float _times[SAMPLES];
-float _pv[SAMPLES];
+float _pv;
+float _time;
+float integratedError = 0;
+float derivedError = 0;
 
 void tune(float kp, float ki, float kd)
 {
@@ -27,61 +25,25 @@ void setPoint(float sp)
 
 void setProcessVariable(float pv, float time)
 {
-	_times[_index] = time;
-	_pv[_index] = pv;
+	auto timeStep = time - _time;
+	integratedError += (_sp - pv) * timeStep;
+	derivedError = (_sp - pv) - (_sp - _pv);
 
-	if (_measurements < SAMPLES)
-	{
-		_measurements += 1;
-	}
-	
-	_index = (_index + 1) % SAMPLES;
+	_time = time;
+	_pv = pv;
 }
 
 float error()
 {
-	return _sp - _pv[(_index - 1 + SAMPLES) % SAMPLES];
-}
-
-float integrateError()
-{
-	//Array is a ring of measurements
-	auto start = _measurements == SAMPLES ? _index : 0;
-
-	float error = 0;
-	for (int i = 0, j = start; i < _measurements - 1; i++, j = (j + 1) % SAMPLES)
-	{
-		//Invariant: error = integrate(start, j, pv over dt)
-		auto timeStep = _times[(j + 1) % SAMPLES] - _times[j];
-		error += (_sp - _pv[j]) * timeStep;
-	}
-	return error;
-}
-
-float derivativeError()
-{
-	//Cant get derivative from a single measurement
-	if (_measurements == 1)
-	{
-		return 0;
-	}
-	auto error1 = _sp - _pv[(_index - 1 + SAMPLES) % SAMPLES];
-	auto error2 = _sp - _pv[(_index - 2 + SAMPLES) % SAMPLES];
-
-	return error1 - error2;
+	return _sp - _pv;
 }
 
 float getManipulatedVariable()
 {
-	if (_measurements == 0) 
-	{
-		return 0;
-	}
-
 	auto p = Kp * error();
-	auto i = Ki * integrateError();
-	auto d = Kd * derivativeError();
-
+	auto i = Ki * integratedError;
+	auto d = Kd * derivedError;
+	
 	return p + i + d;
 }
 
