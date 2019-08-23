7e59feb3adc952ebe51a8190f5da18c4ecb1c24b
John Todd <jqtodd@verisk.com>
2019-08-23 14:50:59 -0600

    Reduce this down futher to be JUST the PID algorithm code

--- thermistor.h
+++
@@ -1,7 +0,0 @@
-//Thermistor
-//Description: Thermistors are a variable resistor, that when hooked up in a voltage divider configuration, we can use the analog pin to read the voltage and convert that into a tempurature.
-//analog: The actual read value from the ADC
-//inCelcius: If you want the returned units to be C/F
-//analogBits: The number of bits in the ADC
-//resistor: The resistance in ohms of the other resistor in the divider circuit
-double thermistor_analogToTemp(int analog, bool inCelcius = true, int analogBits = 12, int resistor = 100000);
